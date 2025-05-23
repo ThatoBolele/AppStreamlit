55e4cd26c9a470572e5d32f949ec71b484bb6e19
Thato Bolele <100345915+ThatoBolele@users.noreply.github.com>
2025-05-23 15:12:25 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -86,7 +86,7 @@ dtype_spec = {
 file_id = "1We-1PSh1KeVJbIDwTw1TRc5FDiMCDcVe"
 url = f"https://drive.google.com/uc?id={file_id}"
 
-df = pd.read_csv(url, dtype=dtype_spec, parse_dates=[ "date"])
+df = pd.read_csv(url, dtype=dtype_spec)
 # df = pd.read_csv("dira_jang.csv")
 df['date'] = pd.to_datetime(df['date'], dayfirst=True)
 df['month'] = df['date'].dt.to_period('M').astype(str)
